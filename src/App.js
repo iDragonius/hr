c48054bb54db5944791a52990785901882e3245e
unknown <leman.miriyeva02@mail.ru>
2022-10-11 10:37:14 +0400

    intial commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import MainLayout from './layouts/mainLayout/MainLayout'
 import AuthLayout from './layouts/authLayout/AuthLayout'
 import SignUp from './pages/auth/signUp/SignUp'
 import SignIn from './pages/auth/signIn/SignIn'
+import CalendarDays from './pages/calendarPage/CalendarDays'
+import StaffAdding from './pages/staffAdding/StaffAdding'
+
 function App() {
     return (
         <div className={'bg-bgPrimary min-h-screen '}>
@@ -15,8 +18,12 @@ function App() {
                 <Route path={'/auth/'} element={<AuthLayout />}>
                     <Route path={'sign-up'} element={<SignUp />} />
                     <Route path={'sign-in'} element={<SignIn />} />
+                    
                 </Route>
-                <Route path={'/'} element={<MainLayout />}></Route>
+                <Route path={'/'} element={<MainLayout />}>
+                <Route path={'calendar'} element={<CalendarDays />} />
+                <Route path={'staff-add'} element={<StaffAdding />} />
+                </Route>
             </Routes>
         </div>
     )
